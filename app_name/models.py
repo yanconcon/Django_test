80f54a0f55e54b36ad82bd5d53c74102cd72bec3
lenovo <1029878802@qq.com>
2017-10-23 20:12:44 +0800

    Default

--- app_name/models.py
+++ app_name/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.aaaaa
 class Person(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField()
